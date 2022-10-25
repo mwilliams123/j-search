d8a0732a46b25c5215d626abd9468b600ab68e75
Megan Williams <wilm140@ucla.edu>
2022-10-24 18:24:34 -0600

    Add DB connection

--- server.js
+++ server.js
@@ -5,12 +5,33 @@ const routes = require("./routes/main");
 
 require("dotenv").config({ path: "./config/.env" });
 
-const config = {
-    user: process.env.SQL_USER,
-    database: process.env.SQL_DATABASE,
-    password: process.env.SQL_PASSWORD,
-    socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
+var config = {
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+};
+
+// Later on when running from Google Cloud, env variables will be passed in container cloud connection config
+if(process.env.NODE_ENV === 'production') {
+  console.log('Running from cloud. Connecting to DB through GCP socket.');
+  config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
 }
+
+// When running from localhost, get the config from .env
+else {
+  console.log('Running from localhost. Connecting to DB directly.');
+  config.host = process.env.DB_HOST;
+}
+
+let connection = mysql.createConnection(config);
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as thread id: ' + connection.threadId);
+});
 
 /*
 let connection = mysql.createConnection({
